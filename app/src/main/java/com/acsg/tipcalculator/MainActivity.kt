195090ef41984c3bf3a4dd7705406f9e3ef7d13a
Andrés Sarmiento <acamilo707@gmail.com>
2023-03-27 20:47:32 -0500

    The compostable to display the amount of the tip value was designed

--- app/src/main/java/com/acsg/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/acsg/tipcalculator/MainActivity.kt
@@ -3,38 +3,79 @@ package com.acsg.tipcalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.acsg.tipcalculator.ui.theme.TipCalculatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TipCalculatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp {
+                TopHeader()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(content: @Composable () -> Unit) {
+    TipCalculatorTheme {
+        // A surface container using the 'background' color from the theme
+        // modifier = Modifier.fillMaxSize()
+        Surface(color = MaterialTheme.colors.background) {
+            content()
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
+@Composable
+fun TopHeader(totalPerPerson: Double = 134.0) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+        color = Color(0xFFE9D7F7)
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Total per person:",
+                style = MaterialTheme.typography.h5
+            )
+            val total = "%.2f".format(totalPerPerson)
+            Text(
+                text = "$$total",
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.ExtraBold
+            )
+        }
+    }
+}
+
+
+//@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    TipCalculatorTheme {
-        Greeting("Android")
+    MyApp {
+        //Text(text = "My app!")
+        //TopHeader()
     }
 }
