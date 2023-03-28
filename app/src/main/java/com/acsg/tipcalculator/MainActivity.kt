7a87247f9e246e3f7b68c9d66df153e931a7be55
Andrés Sarmiento <acamilo707@gmail.com>
2023-03-27 20:54:35 -0500

    Added an early implementation of the input field to enter the total bill value using the InputField custom component

--- app/src/main/java/com/acsg/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/acsg/tipcalculator/MainActivity.kt
@@ -3,20 +3,29 @@ package com.acsg.tipcalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AttachMoney
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.acsg.tipcalculator.components.InputField
 import com.acsg.tipcalculator.ui.theme.TipCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,7 +50,7 @@ fun MyApp(content: @Composable () -> Unit) {
     }
 }
 
-@Preview
+// @Preview
 @Composable
 fun TopHeader(totalPerPerson: Double = 134.0) {
     Surface(
@@ -70,6 +79,43 @@ fun TopHeader(totalPerPerson: Double = 134.0) {
     }
 }
 
+@Preview
+@Composable
+fun MainContent() {
+    val totalBillState = remember { mutableStateOf("0") }
+    val isValidState = remember(totalBillState.value) {
+        try {
+            totalBillState.value.trim().isNotEmpty()
+                    && totalBillState.value.toDouble() >= 0
+        } catch (e: NumberFormatException) {
+            false
+        }
+    }
+    val focusManager = LocalFocusManager.current
+
+    Surface(
+        modifier = Modifier
+            .padding(2.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 2.dp, color = Color.LightGray)
+    ) {
+        Column {
+            InputField(
+                valueState = totalBillState,
+                labelText = "Enter total bill",
+                keyboardType = KeyboardType.Number,
+                onAction = KeyboardActions {
+                    if (!isValidState) return@KeyboardActions
+                    // TODO implement when is valid
+                    focusManager.clearFocus() // hide the keyboard
+                },
+                icon = Icons.Rounded.AttachMoney,
+                iconAlt = "Money icon"
+            )
+        }
+    }
+}
 
 //@Preview(showBackground = true)
 @Composable
